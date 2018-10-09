c57db1e513bf36b68de18d9ff339ee7992891154
Sydney Grant <sg3957@nyu.edu>
2018-10-09 00:37:25 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,12 +1,15 @@
 def get_first_name_of_season_winner(data, season)
-  data.do each |season_number|
-  if season_number == season 
-    season_number.do each |person|
-    if person[:status] == "Winner"
-      person[:status]
-    end
+data.find do |winner|
+    data[:season] == instrcutor_name
   end
-end
+#   data.do each |season_number|
+#   if season_number == season 
+#     season_number.do each |person|
+#     if person[:status] == "Winner"
+#       person[:status]
+#     end
+#   end
+# end
 end
 
 def get_contestant_name(data, occupation)
